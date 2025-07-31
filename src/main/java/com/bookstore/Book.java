e8c53497d72c44137209d017e56555ea626d8e4f
Andrii <stasaaass@gmail.com>
2025-07-31 20:12:39 +0200

    Add main classes: BookRep, BookService, BookContr

---
+++ src/main/java/com/bookstore/Book.java
@@ -0,0 +1,21 @@
+package com.bookstore;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private double price;
+}
